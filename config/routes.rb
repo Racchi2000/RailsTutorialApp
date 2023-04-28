868bdbde54a38431473610b38ef1d04ec1754eb3
堤彬 <tsutsumiakira@13351067207.ap.cc.tsukuba.ac.jp>
2023-04-28 09:06:40 +0900

    ユーザ認証機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
   root 'static_pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
