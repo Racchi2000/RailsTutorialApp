2614151b595a298e794ebad374daf9ca5ef581a1
堤彬 <tsutsumiakira@13351067214.ap.cc.tsukuba.ac.jp>
2023-05-10 13:27:05 +0900

    基本的なツイート機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
   root 'static_pages#home'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  resources :tweets, only:[:create,:destroy]
+  get '/:nickname', to: 'users#show'
 end
